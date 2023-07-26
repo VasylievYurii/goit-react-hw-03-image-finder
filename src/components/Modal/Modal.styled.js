3384cc93a117441278d437a5e8934bf3ffe1674d
VasylievYurii <uraplanetianin@gmail.com>
2023-07-26 16:50:02 +0300

    adaptive for modal

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -13,13 +13,26 @@ export const ModalOverlay = styled.div`
 
 export const ModalWindow = styled.div`
   position: absolute;
+  width: 90%;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+  @media screen and (min-width: 768px) {
+    width: auto;
+  }
 `;
 
 export const ModalImg = styled.img`
-  height: 80vh;
+  width: 100%;
   filter: none;
+  @media screen and (min-width: 400px) {
+  }
+  @media screen and (min-width: 768px) {
+    width: auto;
+    height: 60vh;
+  }
+  @media screen and (min-width: 1200px) {
+    height: 80vh;
+  }
 `;
